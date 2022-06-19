7a7b6fa6e3c35b310997a42cd35cb5ca0d4f0104
Lester Yiu <lester.ws.yiu@gmail.com>
2022-06-19 03:23:37 -0400

    fixed keyboard tabbing issues for currency + product form

--- src/Currencies.js
+++ src/Currencies.js
@@ -1,8 +1,41 @@
+import {useState} from 'react';
+
 const Currencies = (props) => {
     const {setCurrencyChoice} = props;
+    const [checkUSD, setCheckUSD] = useState(false);
+    const [checkCAD, setCheckCAD] = useState(false);
+    const [checkEUR, setCheckEUR] = useState(false);
+    const [checkAUD, setCheckAUD] = useState(false);
+    const [checkGBP, setCheckGBP] = useState(false);
+    const [checkJPY, setCheckJPY] = useState(false);
+    const [checkHKD, setCheckHKD] = useState(false);
+    const [checkKRW, setCheckKRW] = useState(false);
 
     const handleCurrencyClick = (e) => {
         setCurrencyChoice(e.target.value);
+    }
+    const setAllInput = () => {
+        setCheckUSD(false);
+        setCheckCAD(false);
+        setCheckEUR(false);
+        setCheckAUD(false);
+        setCheckGBP(false);
+        setCheckJPY(false);
+        setCheckHKD(false);
+        setCheckKRW(false);
+    }
+
+    const handleCurrency = (setCheckCurrency, currency) => {
+        setAllInput();
+        setCheckCurrency(!currency);
+    }
+
+    const handleKeyPress = (e, handleFunction) => {
+        if (e.code === 'Enter') {
+            setAllInput();
+            handleFunction();
+            setCurrencyChoice(e.target.nextElementSibling.value);
+        }
     }
 
     return (
@@ -10,30 +43,30 @@ const Currencies = (props) => {
             <fieldset>
                 <legend>Choose your currency:</legend>
                 <div className="formInput">
-                    
-                    <label htmlFor="USD">USD </label>
-                    <input type="radio" id="USD" name="currency" value="USD" onChange={handleCurrencyClick}/>
 
-                    <label htmlFor="CAD">CAD</label>
-                    <input type="radio" id="CAD" name="currency" value="CAD" onChange={handleCurrencyClick}/>
+                    <label htmlFor="USD" className={checkUSD ? "activeChoice" : null} onClick={() => {handleCurrency(setCheckUSD, checkUSD)}} tabIndex="0" onKeyPress={(e) => {handleKeyPress(e, () => {handleCurrency(setCheckUSD, checkUSD)})}}>USD </label>
+                    <input type="radio" id="USD" name="currency" value="USD" onChange={handleCurrencyClick} tabIndex="-1"/>
 
-                    <label htmlFor="EUR">EUR</label>
-                    <input type="radio" id="EUR" name="currency" value="EUR" onChange={handleCurrencyClick}/>
+                    <label htmlFor="CAD" className={checkCAD ? "activeChoice" : null} onClick={() => {handleCurrency(setCheckCAD, checkCAD)}}  tabIndex="0" onKeyPress={(e) => {handleKeyPress(e, () => {handleCurrency(setCheckCAD, checkCAD)})}}>CAD</label>
+                    <input type="radio" id="CAD" name="currency" value="CAD" onChange={handleCurrencyClick} tabIndex="-1"/>
 
-                    <label htmlFor="AUD">AUD</label>
-                    <input type="radio" id="AUD" name="currency" value="AUD" onChange={handleCurrencyClick}/>
+                    <label htmlFor="EUR" className={checkEUR ? "activeChoice" : null} onClick={() => {handleCurrency(setCheckEUR, checkEUR)}}  tabIndex="0" onKeyPress={(e) => {handleKeyPress(e, () => {handleCurrency(setCheckEUR, checkEUR)})}}>EUR</label>
+                    <input type="radio" id="EUR" name="currency" value="EUR" onChange={handleCurrencyClick} tabIndex="-1"/>
 
-                    <label htmlFor="GBP">GBP</label>
-                    <input type="radio" id="GBP" name="currency" value="GBP" onChange={handleCurrencyClick}/>
+                    <label htmlFor="AUD" className={checkAUD ? "activeChoice" : null} onClick={() => {handleCurrency(setCheckAUD, checkAUD)}}  tabIndex="0" onKeyPress={(e) => {handleKeyPress(e, () => {handleCurrency(setCheckAUD, checkAUD)})}}>AUD</label>
+                    <input type="radio" id="AUD" name="currency" value="AUD" onChange={handleCurrencyClick} tabIndex="-1"/>
 
-                    <label htmlFor="JPY">JPY</label>
-                    <input type="radio" id="JPY" name="currency" value="JPY" onChange={handleCurrencyClick}/>
+                    <label htmlFor="GBP" className={checkGBP ? "activeChoice" : null} onClick={() => {handleCurrency(setCheckGBP, checkGBP)}}  tabIndex="0" onKeyPress={(e) => {handleKeyPress(e, () => {handleCurrency(setCheckGBP, checkGBP)})}}>GBP</label>
+                    <input type="radio" id="GBP" name="currency" value="GBP" onChange={handleCurrencyClick} tabIndex="-1"/>
 
-                    <label htmlFor="HKD">HKD</label>
-                    <input type="radio" id="HKD" name="currency" value="HKD" onChange={handleCurrencyClick}/>
+                    <label htmlFor="JPY" className={checkJPY ? "activeChoice" : null} onClick={() => {handleCurrency(setCheckJPY, checkJPY)}}  tabIndex="0" onKeyPress={(e) => {handleKeyPress(e, () => {handleCurrency(setCheckJPY, checkJPY)})}}>JPY</label>
+                    <input type="radio" id="JPY" name="currency" value="JPY" onChange={handleCurrencyClick} tabIndex="-1"/>
 
-                    <label htmlFor="KRW">KRW</label>
-                    <input type="radio" id="KRW" name="currency" value="KRW" onChange={handleCurrencyClick}/>
+                    <label htmlFor="HKD" className={checkHKD ? "activeChoice" : null} onClick={() => {handleCurrency(setCheckHKD, checkHKD)}}  tabIndex="0" onKeyPress={(e) => {handleKeyPress(e, () => {handleCurrency(setCheckHKD, checkHKD)})}}>HKD</label>
+                    <input type="radio" id="HKD" name="currency" value="HKD" onChange={handleCurrencyClick} tabIndex="-1"/>
+
+                    <label htmlFor="KRW" className={checkKRW ? "activeChoice" : null} onClick={() => {handleCurrency(setCheckKRW, checkKRW)}}  tabIndex="0" onKeyPress={(e) => {handleKeyPress(e, () => {handleCurrency(setCheckKRW, checkKRW)})}}>KRW</label>
+                    <input type="radio" id="KRW" name="currency" value="KRW" onChange={handleCurrencyClick} tabIndex="-1"/>
                 </div>
             </fieldset>
         </form>
--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,56 @@
+import {useState} from 'react';
+
 const Form = (props) => {
 
     const {setUserInput} = props;
+    const [checkAllProducts, setCheckAllProducts] = useState(false);
+    const [checkPersonal, setCheckPersonal] = useState(false);
+    const [checkShaving, setCheckShaving] = useState(false);
+    const [checkSkinCare, setCheckSkinCare] = useState(false);
+    const [checkOral, setCheckOral] = useState(false);
+    const [checkHairCare, setCheckHairCare] = useState(false);
+    const [checkDeodorant, setCheckDeodorant] = useState(false);
+
+    const setAllInput = () => {
+        setCheckAllProducts(false);
+        setCheckPersonal(false);
+        setCheckShaving(false);
+        setCheckSkinCare(false);
+        setCheckOral(false);
+        setCheckHairCare(false)
+        setCheckDeodorant(false);
+    }
 
     const handleUserChoice = (e) => {
         setUserInput(e.target.value);
+    }
+    const handleProductClick = () => {
+        setAllInput();
+        setCheckAllProducts(!checkAllProducts);
+    }
+    const handlePersonalClick = () => {
+        setAllInput();
+        setCheckPersonal(!checkPersonal);
+    }
+    const handleShavingClick = () => {
+        setAllInput();
+        setCheckShaving(!checkShaving);
+    }
+    const handleSkinCareClick = () => {
+        setAllInput();
+        setCheckSkinCare(!checkSkinCare);
+    }
+    const handleOralClick = () => {
+        setAllInput();
+        setCheckOral(!checkOral);
+    }
+    const handleHairCareClick = () => {
+        setAllInput();
+        setCheckHairCare(!checkHairCare);
+    }
+    const handleDeodorantClick = () => {
+        setAllInput();
+        setCheckDeodorant(!checkDeodorant);
     }
 
     return(
@@ -13,26 +60,26 @@ const Form = (props) => {
 
                 <div className="formInput">
 
-                    <label htmlFor="bathAndBeauty">All Products</label>
-                    <input type="radio" name="type" id="bathAndBeauty" value="Bath & Beauty" onChange={handleUserChoice}/>
+                    <label htmlFor="bathAndBeauty" className={checkAllProducts ? "activeChoice" : null} onClick={handleProductClick} tabIndex="0">All Products</label>
+                    <input type="radio" name="type" id="bathAndBeauty" value="Bath & Beauty" onChange={handleUserChoice} tabIndex="-1"/>
 
-                    <label htmlFor="personalCare">Personal Care</label>
-                    <input type="radio" name="type" id="personalCare" value="Personal Care" onChange={handleUserChoice}/>
+                    <label htmlFor="personalCare" className={checkPersonal ? "activeChoice" : null} onClick={handlePersonalClick} tabIndex="0">Personal Care</label>
+                    <input type="radio" name="type" id="personalCare" value="Personal Care" onChange={handleUserChoice} tabIndex="-1"/>
                     
-                    <label htmlFor="shavingAndGrooming">Shaving & Grooming</label>
-                    <input type="radio" name="type" id="shavingAndGrooming" value="Shaving & Grooming" onChange={handleUserChoice}/>
+                    <label htmlFor="shavingAndGrooming" className={checkShaving ? "activeChoice" : null} onClick={handleShavingClick} tabIndex="0">Shaving & Grooming</label>
+                    <input type="radio" name="type" id="shavingAndGrooming" value="Shaving & Grooming" onChange={handleUserChoice} tabIndex="-1"/>
 
-                    <label htmlFor="skinCare">Skin care</label>
-                    <input type="radio" name="type" id="skinCare" value="Skin Care" onChange={handleUserChoice}/>
+                    <label htmlFor="skinCare" className={checkSkinCare ? "activeChoice" : null} onClick={handleSkinCareClick} tabIndex="0">Skin care</label>
+                    <input type="radio" name="type" id="skinCare" value="Skin Care" onChange={handleUserChoice} tabIndex="-1"/>
 
-                    <label htmlFor="oralCare">Oral Care</label>
-                    <input type="radio" name="type" id="oralCare" value="Oral Care" onChange={handleUserChoice}/>
+                    <label htmlFor="oralCare" className={checkOral ? "activeChoice" : null} onClick={handleOralClick} tabIndex="0">Oral Care</label>
+                    <input type="radio" name="type" id="oralCare" value="Oral Care" onChange={handleUserChoice} tabIndex="-1"/>
 
-                    <label htmlFor="oralCare">Hair Care</label>
-                    <input type="radio" name="type" id="hairCare" value="Hair Care" onChange={handleUserChoice}/>
+                    <label htmlFor="hairCare" className={checkHairCare ? "activeChoice" : null} onClick={handleHairCareClick} tabIndex="0">Hair Care</label>
+                    <input type="radio" name="type" id="hairCare" value="Hair Care" onChange={handleUserChoice} tabIndex="-1"/>
 
-                    <label htmlFor="deodorant">Deodorant</label>
-                    <input type="radio" name="type" id="deodorant" value="Deodorant" onChange={handleUserChoice}/>
+                    <label htmlFor="deodorant" className={checkDeodorant ? "activeChoice" : null} onClick={handleDeodorantClick} tabIndex="0">Deodorant</label>
+                    <input type="radio" name="type" id="deodorant" value="Deodorant" onChange={handleUserChoice} tabIndex="-1"/>
                 </div>
             </fieldset>
         </form>
